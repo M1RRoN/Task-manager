39e4d92f55d0fa747c12631fbce47b3873fe27ff
M1RRoN <mds.myst@gmail.com>
2022-12-12 13:15:32 +0400

    step2

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,9 +3,9 @@ from users import views
 
 urlpatterns = [
     path('', views.users_page, name='users'),
-    path('/create', views.create, name='create_user'),
-    path('/<int:pk>/update/', views.update, name='update_user'),
-    path('/<int:pk>/delete', views.delete, name='delete_user'),
-    path('/login', views.login, name='login'),
-    path('/logout', views.logout, name='logout'),
+    path('create/', views.create, name='create_user'),
+    # path('/<int:pk>/update/', views.update, name='update_user'),
+    # path('/<int:pk>/delete', views.delete, name='delete_user'),
+    path('login/', views.login, name='login'),
+    # path('/logout', views.logout, name='logout'),
 ]
