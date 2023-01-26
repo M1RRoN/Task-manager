9f78f65ade77d7392a945aeddc1a262eb8ac31c2
M1RRoN <mds.myst@gmail.com>
2023-01-26 12:33:06 +0400

    fix task model

--- tasks/models.py
+++ tasks/models.py
@@ -6,12 +6,12 @@ from users.models import CustomUser
 
 class Task(models.Model):
     name = models.CharField(max_length=100, verbose_name="Имя")
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, verbose_name="Описание")
     author = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True)
-    executor = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True, related_name='executor')
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True)
+    executor = models.ForeignKey(CustomUser, on_delete=models.PROTECT, null=True, related_name='executor', verbose_name="Исполнитель")
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, verbose_name="Статус")
     created_at = models.DateTimeField(auto_now_add=True)
-    labels = models.ManyToManyField(Label, through='Relations', blank=True)
+    labels = models.ManyToManyField(Label, through='Relations', blank=True, verbose_name="Метки")
 
     def __str__(self):
         return self.name
