7a50e83c9eae0f0bd0f3c0e360584795a603d38d
M1RRoN <mds.myst@gmail.com>
2023-01-30 19:36:13 +0400

    fix databases

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db`.sqlite3'
+#     }
+# }
 
 DATABASES = dj_database_url.config(
     default='DATABASE_URL', engine='django.db.backends.postgresql'
