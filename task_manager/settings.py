6bea8798737d4f33d4c339e0b6cba3e479d20149
Denis <mds.myst@gmail.com>
2023-02-08 09:31:50 +0400

    fix database

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -99,22 +99,6 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3'
-#     }
-# }
-
-
-# DATABASES = {
-#     "default": dj_database_url.config(conn_max_age=600)
-# }
-
-# my_db = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(my_db)
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
